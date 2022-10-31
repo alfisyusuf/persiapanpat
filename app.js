4c3f3e3c20c41027c5f8ad90e6966984fce6dabe
alfisyusuf <alfisyusuf@gmail.com>
2022-10-31 10:07:33 +0700

    ready awal

---
+++ app.js
@@ -0,0 +1,68 @@
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("bt" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions
+var questions = [
+    new Question("Look at the picture below. The strength of the magnetic field at point P is &hellip; . <br> <img src='https://lh6.googleusercontent.com/z3vpnADVMx7mdmeLCUHWXELuQMuQAJPSNDKaSkUXtENjlfI5RXzwugicGUFV4gmOffm1iamQGbrIcYxGOh4BeC7645dbwrlTayYJzGIMiJJQ7itE2jHZnHWaRlWuPRQBxbW7pXbM1lb3kcT2Hr2HpE7VbkOTuxx2sjHz3AvUq3GSX4U397iuIapKlYM' style='height:145px; width:221px' />", ["1.10<sup>-7</sup> Tesla", "2.10<sup>-7</sup> Tesla","3.10<sup>-7</sup> Tesla", "4.10<sup>-7</sup> Tesla"], "1.10<sup>-7</sup> Tesla"),
+
+    new Question("Look at the picture below. The direction of the magnetic field at point P is &hellip; . <br> <img src='https://lh6.googleusercontent.com/z3vpnADVMx7mdmeLCUHWXELuQMuQAJPSNDKaSkUXtENjlfI5RXzwugicGUFV4gmOffm1iamQGbrIcYxGOh4BeC7645dbwrlTayYJzGIMiJJQ7itE2jHZnHWaRlWuPRQBxbW7pXbM1lb3kcT2Hr2HpE7VbkOTuxx2sjHz3AvUq3GSX4U397iuIapKlYM' style='height:145px; width:221px' />", ["to the right", "to the left","entering the reading screen", "out of the reading screen"], "entering the reading screen"),
+    
+
+    new Question("The electric current I flows on the conductor wire. The magnetic induction at the center of the circle is … . (µ<sub>o</sub> = 4π.10<sup>-7</sup> Wb.m<sup>-1</sup>.A<sup>-1</sup>)<br><img src='https://lh6.googleusercontent.com/rKVXm6i8mnfCX6ggDWKLBcyt7PgMhux_Du8yMd4vauFzmR9AFwuOkOkr6xTKxZTW06Kjkp4rMw-mSPj5lc6HsrIQT9U3bVa0jbM0ujc7ytIxucFPh_pikb3u-3GVzKWX_IIvb2LT6UPKFfz5GY5FLqP8rCY731nLFmr0Y9-n51Z4z1rrcjmidMA1uKM' style='height:239px; width:240px' />", ["4π x 10<sup>-5</sup> Tesla", "3π x 10<sup>-9</sup> Tesla","2π x 10<sup>-5</sup> Tesla", "2π x 10<sup>-7</sup> Tesla"], "2π x 10<sup>-5</sup> Tesla"),
+    
+
+    new Question("A solenoid consists of 1000 windings and is 0.500 cm long. A current of 2.00 amps is supplied to the windings. The magnitude of the strong, constant magnetic field which runs through the center of the solenoid is &hellip; .", ["0.308 T", "0.407 T","0.503 T", "0.500 T"], "0.503 T"),
+        
+
+];
+
+
+var quiz = new Quiz(questions);
+
+
+populate();
+
+
+
+
+
